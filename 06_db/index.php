6831c63f06448126aa0f5f5a3a81a634825e297b
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2020-01-22 20:39:03 +0100

    Update 06_db

--- 06_db/index.php
+++ 06_db/index.php
@@ -42,6 +42,29 @@ foreach($reponse->fetchAll() as $donnees){
     echo "PASSWORD: ".$donnees['password'];
 }
 $reponse->closeCursor(); // Termine le traitement de la requête
+echo '<br>';
+
+// query => Exécute une query SQL et retourne un jeu de résultats en tant qu'objet PDOStatement
+$login = 'admin';
+$reponse = $bdd->query('SELECT * FROM USER WHERE login = "'.$login.'"');
+echo '<br>';
+echo print_r($reponse->fetchAll());
+echo '<br>';
+// prepare => prépare une query SQL et retourne un objet PDOStatement (la query n'est pas exécuter. il faut faire appel à la méthode execute pour cela)
+$reponse = $bdd->prepare('SELECT * FROM USER WHERE login = :login');
+// execute => exécute une query préparée (méthode à exécuter sur un objet PDOStatement)
+$reponse->execute(array(':login' => 'admin'));
+echo '<br>';
+echo print_r($reponse->fetchAll());
+echo '<br>';
+$reponse->execute(array(':login' => 'toto'));
+echo '<br>';
+echo print_r($reponse->fetchAll());
+echo '<br>';
+
+// exec => Exécute une query SQL et retourne le nombre de lignes affectées (ne pas utiliser pour du select)
+echo $bdd->exec('UPDATE USER SET login = \'admin\' WHERE login = \'test\'');
+
 ?>
     </body>
 </html>
