12f708bed72785c8f3657216ec865258ce7d2fc5
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2020-11-30 20:37:03 +0100

    Update 08_router (add trim)

--- 08_router/index.php
+++ 08_router/index.php
@@ -1,9 +1,10 @@
 <?php
 define('ROOT_PATH', "/exercices.php/08_router/"); // Chemin qui suit le nom de domaine. Exemple: http://monprojet.local/08_router/ le path a indiqué sera donc '/08_router/'
 $request = str_replace(ROOT_PATH, "", $_SERVER['REQUEST_URI']); // On récupère juste la request, sans le chemin du dossier.
+$request = trim($request, '/'); // Permer de supprimer le slash devant la request si elle existe
 $segments = array_filter(explode('/', $request)); // On découpe la requête pour obtenir une liste et on supprime les éléments Null
-if (!count($segments) or $segments[0] == 'index'){
-    $segments[0] = 'welcome';
+if (!$segments){
+    $segments[] = 'welcome'; // Si rien dans segments alors on injecte la page "welcome" qui sera la page par défaut (page d'accueil)
 }
 // Structure URL: http://monprojet.be/{REQ_TYPE}/{REQ_TYPE_ID}/{REQ_ACTION}
 define('REQ_TYPE', $segments[0] ?? Null);
